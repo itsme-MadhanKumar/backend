a27875af9ffcd5b92d26ab6f059943a6f52b1c23
Madhan Kumar B <madhandivine3@gmail.com>
2025-01-24 16:17:20 +0530

    third commit

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "https://strong-nasturtium-c9f57a.netlify.app")
 public class ContactController
 {
     @Autowired
